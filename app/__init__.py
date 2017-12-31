5f1900b464fefc011f1cfc82aba5650a1cc89968
developer <lichenlc@gmail.com>
2017-12-31 22:25:55 +0800

    added http auth part

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ import os
 # 3rd party
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+
 
 
 # local import
@@ -15,17 +17,25 @@ from instance.config import app_config
 # initialize sql-alchemy
 db = SQLAlchemy()
 
+# initialize flaskapp
 flaskapp = FlaskAPI(__name__, instance_relative_config=True)
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+secret_key  = os.getenv('SECRET')
 flaskapp.config.from_object(app_config[config_name])
-##app.config.from_pyfile('config.py')
 
+##app.config.from_pyfile('config.py')
+flaskapp.config['SECRET_KEY'] = secret_key
 flaskapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+flaskapp.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db.init_app(flaskapp) #Similar to db = SQLAlchemy(flaskapp)
+
+# initialize http auth
+auth = HTTPBasicAuth()
 
 # Import all models
 # from app.models.Bucketlist import Bucketlist
 
 # Import all app routes
-import app.routes.Bucketlist 
+import app.routes.Bucketlist
+import app.routes.Auth 
 
